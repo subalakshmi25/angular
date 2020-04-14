4f2dcf20d380e8bba826e4146cd024946417ce8e
subav <vijayakumar.vij09@gmail.com>
2020-04-14 18:59:36 +0530

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnChanges, OnInit, 
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,
+AfterViewChecked, OnDestroy{
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    
+  }
+  ngOnInit(): void {
+    alert("2. on init is called");
+  }
+  ngDoCheck(): void {
+    alert("3. do check is called");
+  }
+  ngAfterContentInit(): void {
+    alert("4. after content init called");
+  }
+  ngAfterContentChecked(): void {
+    alert("5. after content check called");
+  }
+  ngAfterViewInit(): void {
+    alert('6. after view init called');
+  }
+ 
+  ngAfterViewChecked(): void {
+    alert('7. after view init checked');
+  }
+
+  ngOnDestroy(): void {
+    alert('8. on destroy called');
+  }
+  title = 'life cycle';
+
+  constructor(){
+    alert("1. on changes called");
+  }
+}
